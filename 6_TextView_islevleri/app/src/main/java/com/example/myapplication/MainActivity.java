33560f3da712faceede2bcdb3c3b044ec5628f92
coderembedded <sevdayucedagg@gmail.com>
2020-09-16 20:21:15 +0300

    ilk android calismalarim

---
+++ 6_TextView_islevleri/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tanimla();
+        goster( "Merhabaaaaaa");
+
+    }
+
+    private void tanimla()
+    {
+        txt = findViewById(R.id.text1);
+    }
+
+    private void goster(String kullaniciAdi)
+    {
+        txt.setText(kullaniciAdi);
+    }
+
+
+
+}
